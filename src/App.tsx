9e4abdf5453f6829c27aaa8cf094213fc3290028
ayelen <ayee_01@live.com>
2022-07-04 01:57:24 -0300

    main

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-    CF SERVICES from branch AYELEN
+    CF SERVICES main
         </p>
         <a
           className="App-link"
